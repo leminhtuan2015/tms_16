39888639a543d789bc8f1b6f3dcc1be276d1f6a6
leminhtuan2015 <leminhtuan2015@gmail.com>
2015-02-10 16:14:03 +0700

    Course-Subject - done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,25 @@ class TasksController < ApplicationController
 	  redirect_to subject_path params[:subject_id]
   end
 
+  def edit
+    @task = Task.find params[:id]
+  end
+
+  def update
+    @task = Task.find params[:id]
+    if @task.update_attributes task_params
+        redirect_to subject_path @task.subject_id
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @task = Task.find params[:id]
+    @task.destroy
+    redirect_to subject_path @task.subject_id
+  end
+
   private
     def task_params
       params.permit(:name, :description, :subject_id)
