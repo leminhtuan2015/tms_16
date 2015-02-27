81bb182dcc151972babf7f1651e1ced95834b3e3
leminhtuan2015 <leminhtuan2015@gmail.com>
2015-02-27 11:11:32 +0700

    Start-course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -34,12 +34,63 @@ class CoursesController < ApplicationController
   end
 
   def update
-    @course = Course.find params[:id]
-    if @course.update_attributes course_params
-      redirect_to course_path @course
+    if params[:course][:start_course]
+      start_course
     else
-      render 'edit'
+      @course = Course.find params[:id]
+      if @course.update_attributes course_params
+        redirect_to course_path @course
+      else
+        render 'edit'
+      end
     end
+  end
+
+  def start_course
+    enrollment_subject
+    enrollment_task
+    redirect_to course_path @course
+  end
+
+  def enrollment_subject
+    @course = Course.find params[:id]
+    @users = @course.users
+    @subjects = @course.subjects
+    @users.each do |user|
+      @subjects.each do |subject|
+        @enrollment_subject = EnrollmentSubject.new
+        @enrollment_subject.course_id = @course.id
+        @enrollment_subject.user_id = user.id
+        @enrollment_subject.subject_id = subject.id
+        if @enrollment_subject.save
+          flash[:success] = "started"
+        else
+          flash[:danger] = "start faild"
+        end
+      end
+    end
+  end
+
+  def enrollment_task
+    @course = Course.find params[:id]
+    @users = @course.users
+    @subjects = @course.subjects
+    @users.each do |user|
+      @subjects.each do |subject|
+        subject.tasks.each do |task|
+          @enrollment_task = EnrollmentTask.new
+          @enrollment_task.course_id = @course.id
+          @enrollment_task.user_id = user.id
+          @enrollment_task.subject_id = subject.id
+          @enrollment_task.task_id = task.id
+          if @enrollment_task.save
+            flash[:success] = "started"
+          else
+            flash[:danger] = "start faild"
+          end
+        end
+      end
+    end 
   end
 
   private
